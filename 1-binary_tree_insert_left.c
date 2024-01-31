302974ac0bfd1638acd0739b9d42f8076fe13417
meta-bay <bayiemeseret@gmail.com>
2024-01-31 09:11:44 +0300

    pre-order traversal

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,6 +23,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
+		return (new_node);
 	}
 	parent->left = new_node;
 	return (new_node);
