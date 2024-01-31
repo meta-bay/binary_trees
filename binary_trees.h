fbdd3b528ae7cb8bfe2225397ecf003b5bafc9ef
meta-bay <bayiemeseret@gmail.com>
2024-01-31 09:24:20 +0300

    in-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -14,10 +14,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -34,6 +34,7 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void print_num(int value);
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
 
