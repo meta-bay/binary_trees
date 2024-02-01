de47ffbdeb58014d0ae25be990499bb63d192f92
meta-bay <bayiemeseret@gmail.com>
2024-02-01 13:29:49 +0300

    trial

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height = 0, left_height = 0;
 	binary_tree_t *temp_left, *temp_right;
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 	temp_left = tree->left;
 	while (temp_left)
