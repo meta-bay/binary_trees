f692c1da39d80ed243c419000116cd90b92f86a1
meta-bay <bayiemeseret@gmail.com>
2024-02-01 17:48:49 +0300

    what if it works

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,6 +14,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
+	binary_tree_is_perfect(tree->left);
+	binary_tree_is_perfect(tree->right);
 	if (left_height == right_height)
 		return (1);
 	return (0);
