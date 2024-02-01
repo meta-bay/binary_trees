e2a6987ce1dac56761e74a998a55656454d85c0d
meta-bay <bayiemeseret@gmail.com>
2024-02-01 12:55:35 +0300

    check

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: the height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right_height = 0, left_height = 0;
+	binary_tree_t *temp_left, *temp_right;
+
+	if (!tree)
+		return (0);
+	temp_left = tree->left;
+	while (temp_left)
+	{
+		left_height++;
+		temp_left = temp_left->left;
+	}
+	temp_right = tree->right;
+	while (temp_right)
+	{
+		right_height++;
+		temp_right = temp_right->right;
+	}
+	if (right_height >= left_height)
+		return (right_height);
+	else
+		return (left_height);
+}
+
