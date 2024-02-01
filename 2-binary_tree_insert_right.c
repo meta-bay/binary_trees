ab1540e22fab9f118f1acba2feceecd6015332ed
meta-bay <bayiemeseret@gmail.com>
2024-02-01 13:14:28 +0300

    fix right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,6 +15,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	newNode = malloc(sizeof(binary_tree_t));
+	if (!newNode)
+		return (NULL);
 	newNode->n = value;
 	newNode->parent = parent;
 	newNode->left = NULL;
