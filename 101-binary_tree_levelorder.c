c992410e1f094c9a71c05b35ca8168baa1b227d9
meta-bay <bayiemeseret@gmail.com>
2024-02-02 05:37:41 +0300

    level-order traversal

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,48 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - goes through a binary tree using level-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a funciton to call for each node
+ */
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	queue_t *begn = malloc(sizeof(queue_t));
+	queue_t *lst = NULL, *rm = NULL;
+
+	if (!tree || !func || !begn)
+		return;
+
+	begn->node = (binary_tree_t *)tree;
+	begn->next = NULL;
+	lst = rm = begn;
+
+	while (begn)
+	{
+		func(begn->node->n);
+		if (begn->node->left)
+		{
+			lst->next = malloc(sizeof(queue_t));
+			if (!lst->next)
+				return;
+			lst = lst->next;
+			lst->node = begn->node->left;
+			lst->next = NULL;
+		}
+		if (begn->node->right)
+		{
+			lst->next = malloc(sizeof(queue_t));
+			if (!lst->next)
+				return;
+			lst = lst->next;
+			lst->node = begn->node->right;
+			lst->next = NULL;
+		}
+		begn = begn->next;
+		free(rm);
+		rm = begn;
+	}
+}
+
+
--- binary_trees.h
+++ binary_trees.h
@@ -25,6 +25,17 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+/**
+ * struct queue - structure for building queue.
+ * @node: pointer to the binary tree
+ * @next: pointer to the next node.
+*/
+typedef struct queue
+{
+	binary_tree_t *node;
+	struct queue *next;
+} queue_t;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -51,6 +62,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
 
