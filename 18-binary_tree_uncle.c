0b6848ac80c5a2134b27062c324690007c84f924
meta-bay <bayiemeseret@gmail.com>
2024-02-02 05:02:40 +0300

    betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_unlce - finds the uncle of a node
+ * binary_tree_uncle - finds the uncle of a node
  * @node: pointer to the node to find the uncle
  * Return: the uncle
  */
@@ -10,10 +10,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-
-	return (binary_tree_sibling(node->parent);
+	return (binary_tree_sibling(node->parent));
 }
-
 
 /**
  * binary_tree_sibling - finds the sibling of a node
@@ -23,13 +21,12 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        if (!node || !node->parent)
-                return (NULL);
-
-        if (node->parent->left == node)
-                return (node->parent->right);
-        if (node->parent->right == node)
-                return (node->parent->left);
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	if (node->parent->right == node)
+		return (node->parent->left);
+	return (NULL);
 }
 
