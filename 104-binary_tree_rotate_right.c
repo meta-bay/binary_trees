7fd3408f7dad40a305baf928e411db95fb58bd6f
meta-bay <bayiemeseret@gmail.com>
2024-02-02 05:56:27 +0300

    rotate right

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate - right-rotation
+ * @tree: pointer to the root node of the tree to rotate
+ * Return: the new root
+ */
+
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *new_root_node;
+
+	if (!tree)
+		return (NULL);
+
+	new_root_node = tree->left;
+	tree->left = new_root_node->right;
+	if (tree->left)
+		tree->left->parent = tree;
+	new_root_node->right = tree;
+	new_root_node->parent = tree->parent;
+	tree->parent = new_root_node;
+	return (new_root_node);
+}
+
