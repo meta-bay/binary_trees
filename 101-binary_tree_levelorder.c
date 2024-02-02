4749e85f8ca323396996b652d2d02b53624cafb3
meta-bay <bayiemeseret@gmail.com>
2024-02-02 05:39:25 +0300

    betty

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_levelorder - goes through a binary tree using level-order traversal
+ * binary_tree_levelorder - goes through a binary tree
+ * using level-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a funciton to call for each node
  */
